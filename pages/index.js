e414803cd52144893e106523ce789d0f324b080e
tsg2025 <tradingstrategyguru@gmail.com>
2025-04-02 08:39:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Welcome to Stock Data App</h1>
-      <p>Fetch and store historical stock data efficiently using IndexedDB.</p>
-      <Link href="/stocks">
-        <button style={{ padding: "10px", fontSize: "16px", cursor: "pointer" }}>
-          Go to Stock Data Page
-        </button>
-      </Link>
+    <div style={{ padding: "20px" }}>
+      <h1>Stock App</h1>
+      <p>Welcome! Go to the <a href="/stocks">Stock Page</a> to fetch stock data.</p>
     </div>
   );
 }
