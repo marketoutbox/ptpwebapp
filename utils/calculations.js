9d6c3f5cc5b77f6573d82bd778a2f46d6b5ebc06
tsg2025 <tradingstrategyguru@gmail.com>
2025-04-03 18:48:31 +0530

    Update calculations.js

--- utils/calculations.js
+++ utils/calculations.js
@@ -1,4 +1,4 @@
-export function calculateZScore(values) {
+export default function calculateZScore(values) {
     const mean = values.reduce((sum, val) => sum + val, 0) / values.length;
     const stdDev = Math.sqrt(
         values.reduce((sum, val) => sum + Math.pow(val - mean, 2), 0) / values.length
