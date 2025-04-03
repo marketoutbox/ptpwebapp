3aab0a143fa1cf950b682a1c379ff8a2fed51ad1
tsg2025 <tradingstrategyguru@gmail.com>
2025-04-03 18:32:13 +0530

    Update backtest.js

--- pages/backtest.js
+++ pages/backtest.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { openDB } from 'idb';
-import { calculateZScore } from '../utils/calculateZScore';
+import { calculateZScore } from '../utils/calculations';
 
 const Backtest = () => {
   const [stocks, setStocks] = useState([]);
